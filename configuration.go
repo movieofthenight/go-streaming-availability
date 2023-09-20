59119180594487777c00affe5bb6ad86c1cc11b5
The Meastro <134454779+the-maestro-bot@users.noreply.github.com>
2023-09-21 00:07:18 +0200

    Regenerate the client library according to the new specs

--- configuration.go
+++ configuration.go
@@ -89,7 +89,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "go-streaming-availability/2.1.0",
+		UserAgent:        "go-streaming-availability/v2.1.1",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
